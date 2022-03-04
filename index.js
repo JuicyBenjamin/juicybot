e35c7fde7520b8f35a7dfb42b6a0d3596c8f8d54
Benjamin Vinterberg <benj924c@stud.kea.dk>
2022-03-04 20:15:23 +0100

    See if it works

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.client();
-const config = require("./config.json");
+const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("The client is ready!");
