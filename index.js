1c169c5f281f932517a226029d195efd1b8bb434
Benjamin Vinterberg <benj924c@stud.kea.dk>
2022-03-04 20:40:42 +0100

    Updated discordjs lingo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+import DiscordJS, { Intents } from "discord.js";
+const client = new DiscordJS.Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.on("ready", () => {
   console.log("The client is ready!");
