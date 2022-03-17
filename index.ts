2553ac46a91805fbd44798d4decf96e07d9c004c
Benjamin Vinterberg <benj924c@stud.kea.dk>
2022-03-17 19:43:33 +0100

    add copypasta command

--- index.ts
+++ index.ts
@@ -1,57 +1,69 @@
-import * as DiscordJS from 'discord.js'
-import { Intents } from 'discord.js'
-import axios from 'axios'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as DiscordJS from "discord.js";
+import { Intents } from "discord.js";
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const client = new DiscordJS.Client({
-  partials: ['CHANNEL'],
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_TYPING],
-})
+  partials: ["CHANNEL"],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+  ],
+});
 
-client.on('ready', () => {
-  console.log('The bot is ready')
+client.on("ready", () => {
+  console.log("The bot is ready");
   if (client.user) {
-    client.user.setActivity('The demise of Botnex!', { type: 'WATCHING' })
+    client.user.setActivity("The demise of Botnex!", { type: "WATCHING" });
   } else {
-    console.log('Benji you idiot, this fucking thing is null')
+    console.log("Benji you idiot, this fucking thing is null");
   }
-})
+});
 
-client.on('messageCreate', (message) => {
-  if (message.content.toLocaleLowerCase() === 'ping') {
+client.on("messageCreate", (message) => {
+  if (message.content.toLocaleLowerCase() === "ping") {
     message.reply({
-      content: 'pong',
-    })
+      content: "pong",
+    });
   }
-  if (message.content.toLocaleLowerCase() === 'b') {
+  if (message.content.toLocaleLowerCase() === "b") {
     message.reply({
-      content: 'ß',
-    })
+      content: "ß",
+    });
   }
 
-  if (message.content.toLocaleLowerCase() === '!pasta') {
+  if (message.content.toLocaleLowerCase() === "!pasta") {
     axios.get(`${process.env.COPYPASTA}stories.json`).then((resp) => {
-      Object.keys(resp.data).forEach((key) => {
-        console.log(key.story)
-      })
-    })
-    // message.channel.send("Once JIB has made a library of copy pasta I'll start sending them")
+      const stories = Object.values(resp.data);
+      const randStory: any =
+        stories[Math.floor(Math.random() * stories.length)];
+      const story = "story";
+      if (story in randStory) {
+        //console.log(randStory.story);
+        message.channel.send(randStory.story);
+      }
+    });
   }
 
-  if (message.channel.type == 'DM') {
-    console.log(message.author)
-    if (message.author.id == process.env.JIB || message.author.id == process.env.JUICYBENJAMIN) {
-      console.log(message.content)
+  if (message.channel.type == "DM") {
+    console.log(message.author);
+    if (
+      message.author.id == process.env.JIB ||
+      message.author.id == process.env.JUICYBENJAMIN
+    ) {
+      console.log(message.content);
       axios
         .post(`${process.env.COPYPASTA}stories.json`, {
           story: message.content,
         })
         .then((response) => {
-          console.log(response)
-        })
+          console.log(response);
+        });
     }
   }
-})
+});
 
-client.login(process.env.DJS_TOKEN)
+client.login(process.env.DJS_TOKEN);
